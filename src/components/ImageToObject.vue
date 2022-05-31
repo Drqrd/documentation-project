acbe2b8db7dbecb3d6533d0c6641c69346494d8d
justin_k_m <justinm0623@gmail.com>
2022-05-31 15:14:21 -0400

    Create ImageToObject.vue

---
+++ src/components/ImageToObject.vue
@@ -0,0 +1,23 @@
+<template>
+    <img :src="image"/>
+</template>
+
+<script>
+
+export default {
+    name:'ImageToObject',
+    props: {
+        image:{
+            type: String,
+        },
+    }
+}
+</script>
+
+<style scoped>
+img {
+    max-height: 64px;
+    max-width: 64px;
+}
+
+</style>
