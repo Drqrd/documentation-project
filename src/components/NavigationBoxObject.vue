267bf08f2a39857767ffda3ab86c70d79ff7e4a4
justin_k_m <justinm0623@gmail.com>
2022-05-31 15:14:25 -0400

    Create NavigationBoxObject.vue

---
+++ src/components/NavigationBoxObject.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <ImageToObject :image="icons.VueIcon"></ImageToObject>
+    </div>
+</template>
+
+<script>
+import ImageToObject from '../components/ImageToObject.vue'
+
+import VueIcon from '../icons/icon_vue.png'
+
+
+export default {
+    name: 'NavigationBoxObject',
+    components: {
+        ImageToObject,
+    },
+    data: ()=> ({
+        icons:{
+            VueIcon
+        }
+    })
+}
+</script>
